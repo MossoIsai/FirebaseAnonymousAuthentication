12b3cc68c01020271cb3d840316ef79bc98ab20f
Alex Mamo <alexmamo@gmail.com>
2023-01-16 14:16:16 +0200

    Minor fixes and improvements.

--- app/src/main/java/ro/alexmamo/firebaseanonymousauthentication/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/ro/alexmamo/firebaseanonymousauthentication/data/repository/AuthRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package ro.alexmamo.firebaseanonymousauthentication.data.repository
 
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 import ro.alexmamo.firebaseanonymousauthentication.domain.model.Response.Failure
 import ro.alexmamo.firebaseanonymousauthentication.domain.model.Response.Success
 import ro.alexmamo.firebaseanonymousauthentication.domain.repository.AuthRepository
@@ -16,11 +18,13 @@ class AuthRepositoryImpl  @Inject constructor(
     override val isUserAuthenticatedInFirebase = auth.currentUser != null
 
     override suspend fun firebaseSignInAnonymously(): SignInResponse {
-        return try {
-            auth.signInAnonymously().await()
-            Success(true)
-        } catch (e: Exception) {
-            Failure(e)
+        return withContext(Dispatchers.IO) {
+            try {
+                auth.signInAnonymously().await()
+                Success(true)
+            } catch (e: Exception) {
+                Failure(e)
+            }
         }
     }
 }
